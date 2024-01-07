ac55585964a551d99332ea4204cea7084e016cac
Mukesh-P <132244197+Mukesh-P@users.noreply.github.com>
2024-01-07 18:52:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ with st.container():
     st.write('##')
 
     contact_form = """
-    <form action="https://formsubmit.co/Mukesh.testuser01@gmail.com" method="POST">
+    <form action="https://formsubmit.co/mukesh.testuser01@gmail.com" method="POST">
         <input type ="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your Name" required>
         <input type="email" name="email" placeholder="Your Email" required>
